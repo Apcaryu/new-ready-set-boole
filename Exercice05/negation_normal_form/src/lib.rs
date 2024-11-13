a80677e310aa1a06f5c8c26a1cf6bb891198dc14
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2024-11-13 17:03:59 +0100

    Ex05 - refactor symbol function

--- Exercice05/negation_normal_form/src/lib.rs
+++ Exercice05/negation_normal_form/src/lib.rs
@@ -1,82 +1,79 @@
-// fn get_a_b(){}
+#[derive(PartialEq)]
+enum Symbol {
+	Negation,
+	Conjunction,
+	Disjunction,
+	ExclusiveDisjunction,
+	MaterialCondition,
+	LogicalEquivalence,
+}
 
-fn negation(stack: &mut Vec<String>) {
-	if stack.len() == 0 {
-		panic!("missing variable");
-	}
+fn negation(var_a: String) -> String {
+	let res;
 
-	let tmp = stack.pop();
-	let mut res = String::new();
-
-	match tmp {
-		Some(val) => {
-			if val.chars().last() != Some('!') {
-				res = String::from(format!("{}!", val));
+			if var_a.chars().last() != Some('!') {
+				res = String::from(format!("{}!", var_a));
 			} else {
-				let mut tmp_res = val.chars();
+				let mut tmp_res = var_a.chars();
 				tmp_res.next_back();
 				res = String::from(tmp_res.as_str())
 			}
+
+	res
+}
+
+fn conjunction(var_a: String, var_b: String) -> String {
+	format!("{}{}&", var_a, var_b)
+}
+
+fn disjunction(var_a: String, var_b: String) -> String {
+	format!("{}{}|", var_a, var_b)
+}
+
+fn exclusive_disjunction(var_a: String, var_b: String) -> String {
+	format!("{}{}!&{}!{}&|", var_a, var_b, var_a, var_b)
+}
+
+fn material_condition(var_a: String, var_b: String) -> String {
+	format!("{}!{}|", var_a, var_b)
+}
+
+fn logical_equivalence(var_a: String, var_b: String) -> String {
+	format!("{}{}&{}!{}!&|", var_a, var_b, var_a, var_b)
+}
+
+fn nnf_for_one_symbol(symbol:Symbol, stack: &mut Vec<String>) {
+	let var_a;
+	let var_b ;
+
+	if symbol == Symbol::Negation {
+		var_a = stack.pop().unwrap();
+		var_b = String::new();
+	} else {
+		var_b = stack.pop().unwrap();
+		var_a = stack.pop().unwrap();
+	}
+
+	match symbol {
+		Symbol::Negation => {
+			stack.push(negation(var_a));
 		},
-		_ => {}
+		Symbol::Conjunction => {
+			stack.push(conjunction(var_a, var_b));
+		},
+		Symbol::Disjunction => {
+			stack.push(disjunction(var_a, var_b));
+		},
+		Symbol::ExclusiveDisjunction => {
+			stack.push(exclusive_disjunction(var_a, var_b));
+		},
+		Symbol::MaterialCondition => {
+			stack.push(material_condition(var_a, var_b));
+		},
+		Symbol::LogicalEquivalence => {
+			stack.push(logical_equivalence(var_a, var_b));
+		},
 	}
-
-	stack.push(res);
-}
-
-fn conjunction(stack: &mut Vec<String>) {
-	if stack.len() < 2 {
-		panic!("missing variable")
-	}
-
-	let var_b = stack.pop().unwrap();
-	let var_a = stack.pop().unwrap();
-
-	stack.push(String::from(format!("{}{}&", var_a, var_b)));
-}
-
-fn disjunction(stack: &mut Vec<String>) {
-	if stack.len() < 2 {
-		panic!("missing variable")
-	}
-
-	let var_b = stack.pop().unwrap();
-	let var_a = stack.pop().unwrap();
-
-	stack.push(String::from(format!("{}{}|", var_a, var_b)));
-}
-
-fn exclusive_disjunction(stack: &mut Vec<String>) {
-	if stack.len() < 2 {
-		panic!("missing variable")
-	}
-
-	let var_b = stack.pop().unwrap();
-	let var_a = stack.pop().unwrap();
-
-	stack.push(String::from(format!("{}{}!&{}!{}&|", var_a, var_b, var_a, var_b)));
-}
-
-fn material_condition(stack: &mut Vec<String>) {
-	if stack.len() < 2 {
-		panic!("missing variable")
-	}
-
-	let var_b = stack.pop().unwrap();
-	let var_a = stack.pop().unwrap();
-
-	stack.push(String::from(format!("{}!{}|", var_a, var_b)));
-}
-
-fn logical_equivalence(stack: &mut Vec<String>) {
-	if stack.len() < 2 {
-		panic!("missing variable")
-	}
-
-	let var_b = stack.pop().unwrap();
-	let var_a = stack.pop().unwrap();
-
-	stack.push(String::from(format!("{}{}&{}!{}!&|", var_a, var_b, var_a, var_b)));
 }
 
 pub fn negation_normal_form(formula: &str) -> String {
@@ -88,22 +85,22 @@ pub fn negation_normal_form(formula: &str) -> String {
 				stack.push(String::from(chr));
 			},
 			'!' => {
-				negation(&mut stack);
+				nnf_for_one_symbol(Symbol::Negation, &mut stack);
 			},
 			'&' => {
-				conjunction(&mut stack);
+				nnf_for_one_symbol(Symbol::Conjunction, &mut stack);
 			},
 			'|' => {
-				disjunction(&mut stack);
+				nnf_for_one_symbol(Symbol::Disjunction, &mut stack);
 			},
 			'^' => {
-				exclusive_disjunction(&mut stack);
+				nnf_for_one_symbol(Symbol::ExclusiveDisjunction, &mut stack);
 			},
 			'>' => {
-				material_condition(&mut stack);
+				nnf_for_one_symbol(Symbol::MaterialCondition, &mut stack);
 			},
 			'=' => {
-				logical_equivalence(&mut stack);
+				nnf_for_one_symbol(Symbol::LogicalEquivalence, &mut stack);
 			},
 			_ => {
 				panic!("invalid input")
