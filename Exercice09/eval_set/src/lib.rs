251204649c38d7304752775490d2aeb03144ae11
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2024-12-17 15:04:05 +0100

    Ex09 - variable cases works

--- Exercice09/eval_set/src/lib.rs
+++ Exercice09/eval_set/src/lib.rs
@@ -1,4 +1,26 @@
+use negation_normal_form::negation_normal_form;
+
+fn get_set(stack: &mut Vec<Vec<i32>>, chr: char, sets: &Vec<Vec<i32>>) {
+	let idx = chr.to_digit(16).unwrap() - 10; // 10 is the value of A
+	stack.push(sets[idx as usize].clone());
+}
+
 pub fn eval_set(formula: &str, sets: Vec<Vec<i32>>) -> Vec<i32> {
+	let nnf_formula = negation_normal_form(formula);
+	let mut stack = Vec::new();
+
+	for chr in nnf_formula.chars() {
+		match chr {
+			'A'..='Z' => {
+				get_set(&mut stack, chr, &sets);
+			},
+			'!' => {},
+			'&' => {},
+			'|' => {},
+			_ => {}
+		}
+	}
+	println!("stack {:?}", stack);
     vec![]
 }
 
